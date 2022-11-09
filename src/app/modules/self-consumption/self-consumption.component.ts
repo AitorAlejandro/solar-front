1550dca3505479f996358faa6b21fa30f5b4fb72
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2022-11-09 19:42:19 +0100

    self-consumption module

---
+++ src/app/modules/self-consumption/self-consumption.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-self-consumption',
+  templateUrl: './self-consumption.component.html',
+  styleUrls: ['./self-consumption.component.scss'],
+})
+export class SelfConsumptionComponent {
+  constructor() {}
+}
